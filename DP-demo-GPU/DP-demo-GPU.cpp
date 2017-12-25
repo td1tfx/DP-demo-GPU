76175a0b37d88302594c583064118ff24d5024e2
tfx <td1tfx@163.com>
2017-12-25 11:57:16 +0900

    openblas

--- DP-demo-GPU/DP-demo-GPU.cpp
+++ DP-demo-GPU/DP-demo-GPU.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
+#include "cblas.h"
+#include <stdlib.h>
+#include <iostream>
+
 
 
 int main()
 {
+	float a[1] = { 2 };
+	float b[1] = { 3 };
+	float c[1] = { 0 };
+	cblas_sgemm(CblasRowMajor,
+		CblasNoTrans, CblasNoTrans,
+		1, 1, 1,
+		1,
+		a, 1,
+		b, 1,
+		0,
+		c, 1);
+	std::cout << *c << std::endl;
+	system("pause");
     return 0;
 }
 
